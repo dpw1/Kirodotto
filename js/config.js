17380435aada6699a72bbb25b6db5e324df86dfe
dpw1 <diego.boarutto.fortes@gmail.com>
2025-05-21 18:37:24 -0300

    heigh tests

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  version: "1.09",
+  version: "1.10",
   minBallSize: 15, // Absolute minimum size for any ball
   maxBallSize: 200, // Absolute maximum size for any ball
   ballSizeRange: [20, 40], // Range for initial ball size randomization [min, max]
